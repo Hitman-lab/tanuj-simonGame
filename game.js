a1a955de3a54630a450d4656c72e2797d76485d6
Desktop-PC <hackzs@outlook.com>
2020-04-19 00:28:10 +0530

    .

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
 let buttonColurs = ['red','blue','green', 'yellow'];
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
 
 $('.btns').click(function () {
     // store the ID of the button tha got clicked
     let userChosenColour = $(this).attr('id');
     userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);    
+    playSound(userChosenColour);  
+    animationPress(userChosenColour);  
 });
 
 function nextSequence() {
@@ -20,4 +22,13 @@ function nextSequence() {
 function playSound(musicName) {
     let audio = new Audio('sounds/' + musicName + '.mp3');
     audio.play();
+}
+
+function animationPress(currentColour) {
+    let colour = $('#' + currentColour);
+    colour.addClass('pressed');
+    
+    setTimeout(function() {
+        colour.removeClass('pressed');
+    }, 50);
 }
